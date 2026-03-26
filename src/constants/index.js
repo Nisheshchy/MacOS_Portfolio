5f23dd730e734903ed290b09d065c7fc353b981c
Nisheshchy <nisheshchy2022@gmail.com>
2026-03-26 23:17:08 +0545

    Update constants/index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -248,7 +248,7 @@ const WORK_LOCATION = {
     // ▶ Project 1
     {
       id: 5,
-      name: "Nike Ecommerce Website Application",
+      name: "Car World Website",
       icon: "/images/folder.png",
       kind: "folder",
       position: "top-10 left-5", // icon position inside Finder
@@ -256,30 +256,30 @@ const WORK_LOCATION = {
       children: [
         {
           id: 1,
-          name: "Nike Project.txt",
+          name: "Car Website.txt",
           icon: "/images/txt.png",
           kind: "file",
           fileType: "txt",
           position: "top-5 left-10",
           description: [
-            "The Nike eCommerce website is a sleek and modern platform designed for shopping the latest Nike collections.",
+            "The Car Website is a sleek and modern platform designed for shopping the latest Car collections.",
             "Instead of a simple online store, it delivers an immersive experience with bold visuals, interactive product displays, and smooth navigation.",
-            "Think of it like walking into a flagship Nike store—but right from your phone or laptop.",
+            "Think of it like walking into a flagship Car store—but right from your phone or laptop.",
             "It's built with Next.js and Tailwind, ensuring fast performance, responsive design, and a clean, premium look.",
           ],
         },
         {
           id: 2,
-          name: "nike.com",
+          name: "car-website.com",
           icon: "/images/safari.png",
           kind: "file",
           fileType: "url",
-          href: "https://youtu.be/fZdTYswuZjU?si=Awjl-pIst9e09_UU",
+          href: "https://carworld-sandy.vercel.app/",
           position: "top-10 right-20",
         },
         {
           id: 4,
-          name: "nike.png",
+          name: "car-website.png",
           icon: "/images/image.png",
           kind: "file",
           fileType: "img",
@@ -354,7 +354,7 @@ const WORK_LOCATION = {
     // ▶ Project 3
     {
       id: 7,
-      name: "Food Delivery App",
+      name: "Food App",
       icon: "/images/folder.png",
       kind: "folder",
       position: "top-10 left-80",
@@ -362,30 +362,30 @@ const WORK_LOCATION = {
       children: [
         {
           id: 1,
-          name: "Food Delivery App Project.txt",
+          name: "Food App Project.txt",
           icon: "/images/txt.png",
           kind: "file",
           fileType: "txt",
           position: "top-5 left-10",
           description: [
-            "Our Food Delivery App is a fast and convenient way to order meals from your favorite restaurants.",
-            "Instead of making calls or waiting in line, you can browse menus, customize orders, and track deliveries in real time.",
-            "Think of it like having your favorite restaurants in your pocket—ready to deliver anytime, anywhere.",
-            "It’s built with React Native, so it works smoothly on both iOS and Android with a clean, modern design.",
+            "Our Food App lets you order meals quickly from your favorite restaurants.",
+            "You browse menus, customize your order, and place it in a few taps.",
+            "You track your delivery in real time, so you always know when your food arrives.",
+            "The app removes the need for calls or waiting in line.",
           ],
         },
         {
           id: 2,
-          name: "food-delivery-app.com",
+          name: "food-app.com",
           icon: "/images/safari.png",
           kind: "file",
           fileType: "url",
-          href: "https://youtu.be/LKrX390fJMw?si=cExkuVhf2DTV9G2-",
+          href: "https://foodapp2-red.vercel.app/",
           position: "top-10 right-20",
         },
         {
           id: 4,
-          name: "food-delivery-app.png",
+          name: "food-app.png",
           icon: "/images/image.png",
           kind: "file",
           fileType: "img",
